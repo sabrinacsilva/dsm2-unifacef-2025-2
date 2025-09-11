a2993ca1b2848fe01ef824509eb0ecf7a26897ee
sabrinacsilva <129235747+sabrinacsilva@users.noreply.github.com>
2025-09-11 00:40:33 +0000

    Primeiro commit

--- Aula03/App.js
+++ Aula03/App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Configuração do ambiente de desenvolvimento - Aula 03</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>📱 Aula 03 - DSM2</Text>
+      <Text style={styles.subtitle}>
+        Projeto modificado com novo texto e estilo!
+      </Text>
+      <Text style={styles.footer}>Aluno: Sabrina Costa</Text>
     </View>
   );
 }
@@ -13,8 +17,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e3f2fd', // azul claro
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#0d47a1',
+    marginBottom: 10,
+    textTransform: 'uppercase',
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#1565c0',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  footer: {
+    fontSize: 14,
+    fontStyle: 'italic',
+    color: '#555',
   },
 });
